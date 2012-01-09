5f8268676a62216aa2a0a6c3eff18123559aac01
oritomov <oritomov@ea482eac-fd92-57f8-a034-e20fc9fb603f>
2012-01-09 18:38:45 +0000

    minor fix

--- src/com/google/code/autowiring/tynicad/TyniCAD.java
+++ src/com/google/code/autowiring/tynicad/TyniCAD.java
@@ -33,9 +33,7 @@ public class TyniCAD extends Xml implements Wiring {
 
 	private TiniCadConfig engine;
 	private Node dsnRoot;
-//	private Color wireColor = new Color(0);
 //	private Font pinFont;
-//	private Color pinColor = new Color(0);
 //	private Map<Node, Position> stored = new LinkedHashMap<Node, Position>();
 //	private Position origin = new Position(0, 0);
 	private List<Bean> refs;
@@ -56,10 +54,8 @@ public class TyniCAD extends Xml implements Wiring {
 		refs.add(options);
 		/** extract all other */
 		beans = ceateBeans(tiny.getFirstChild());
-//		wireColor = getColor(getNode(options, COLOR_WIRE).getTextContent());
 //		pinFont = getFont(find(dsnRoot, FONT, "1"));
 //		pinFont = pinFont.deriveFont(3.0f);
-//		pinColor = getColor(getNode(options, COLOR_PIN).getTextContent());
 	}
 
 	@Override
@@ -239,138 +235,6 @@ public class TyniCAD extends Xml implements Wiring {
 //		child = entry.getKey();
 //		origin = entry.getValue();
 //		stored.remove(child);
-//	}
-
-//	private Bean symbol(Node node) {
-//		String pos = getAttrValue(node, "pos");
-//		double x = Double.parseDouble(pos.split(",")[0]);
-//		double y = Double.parseDouble(pos.split(",")[1]);
-//		String id = getAttrValue(node, ID);
-//		Node defRoot = find(dsnRoot, SYMBOLDEF, id);
-//		Node refPoint = getNode(defRoot, REF_POINT);
-//		pos = getAttrValue(refPoint, "pos");
-//		x -= Double.parseDouble(pos.split(",")[0]);
-//		y -= Double.parseDouble(pos.split(",")[1]);
-//		Position displace = new Position(x, y);
-//		Node def = getNode(defRoot, TINY_CAD);
-//		Node defChild = def.getFirstChild();
-//		store(defChild, displace);
-//		Bean bean = nextBean();
-//		return bean;
-//	}
-
-//	private Node find(Node dsnRoot, String name, String id) {
-//		Node tiny = getNode(dsnRoot, TINY_CAD);
-//		Node node = getNode(tiny, name);
-//		String nodeId = getAttrValue(node, ID);
-//		while (!id.equalsIgnoreCase(nodeId)) {
-//			node = nextNode(node, name);
-//			nodeId = getAttrValue(node, ID);
-//		}
-//		return node;
-//	}
-
-//	private Fill fill(Node node) {
-//		int fillCode = Integer.parseInt(getNode(node, "INDEX").getTextContent());
-//		FillInx fillInx = FillInx.get(fillCode);
-//		Color color = getColor(getNode(node, COLOR).getTextContent());
-//		Fill fill = new Fill(fillInx, color);
-//		return fill;
-//	}
-
-//	private Rectangle rectangle(Node root, Node node) {
-//		String a = getAttrValue(node, "a");
-//		double x = Double.valueOf(a.split(",")[0]);
-//		double y = Double.valueOf(a.split(",")[1]);
-//		String b = getAttrValue(node, "b");
-//		double x2 = Double.valueOf(b.split(",")[0]);
-//		double y2 = Double.valueOf(b.split(",")[1]);
-//		String styleId = getAttrValue(node, STYLE);
-//		Node style = find(root, STYLE, styleId);
-//		Color color = getColor(getNode(style, COLOR).getTextContent());
-//		int thickness = Integer.parseInt(getNode(style, THICKNESS).getTextContent());
-//		String fillId = getAttrValue(node, FILL);
-//		Fill fill = fill(find(root, FILL, fillId));
-//		//Rectangle rectangle = new Rectangle(Math.min(x,x2)+origin.getX(), Math.min(y,y2)+origin.getY(), Math.max(x,x2)+origin.getX(), Math.max(y,y2)+origin.getY(), fill, color, thickness);
-//		Rectangle rectangle = new Rectangle(x+origin.getX(), y+origin.getY(), x2+origin.getX(), y2+origin.getY(), fill, color, thickness);
-//		return rectangle;
-//	}
-
-//	private Bean polygon(Node node) {
-//		String pos = getAttrValue(node, "pos");
-//		double x = Double.parseDouble(pos.split(",")[0]);
-//		double y = Double.parseDouble(pos.split(",")[1]);
-//		// style
-//		// fill
-//		Polygon polygon = new Polygon(x, y);
-//		Node p = getNode(node, POINT);
-//		while (p != null) {
-//			pos = getAttrValue(p, "pos");
-//			x = Double.parseDouble(pos.split(",")[0]);
-//			y = Double.parseDouble(pos.split(",")[1]);
-//			int arcCode = Integer.parseInt(getAttrValue(p, "arc"));
-//			Arc arc = Arc.get(arcCode);
-//			Point point = new Point(x, y, arc);
-//			polygon.add(point);
-//			p = nextNode(p, POINT);
-//		}
-//		return polygon;
-//	}
-
-//	private Label label(Node root, Node node) {
-//		String pos = getAttrValue(node, "pos");
-//		double x = Double.parseDouble(pos.split(",")[0]);
-//		double y = Double.parseDouble(pos.split(",")[1]);
-//		int directionCode = Integer.parseInt(getAttrValue(node, "direction"));
-//		Direction direction = Direction.get(directionCode);
-//		Color color = getColor(getAttrValue(node, COLOR));
-//		String fontId = getAttrValue(node, "font");
-//		Font font = getFont(find(root, FONT, fontId));
-//		//String style = Xml.getAttrValue(child, "style");
-//		String text = node.getTextContent();
-//		Label label = new Label(x+origin.getX(), y+origin.getY(), color, direction, font, text);
-//		return label;
-//	}
-
-//	private Wire wire(Node node) {
-//		String a = getAttrValue(node, "a");
-//		double x1 = Double.parseDouble(a.split(",")[0]);
-//		double y1 = Double.parseDouble(a.split(",")[1]);
-//		String b = getAttrValue(node, "b");
-//		double x2 = Double.parseDouble(b.split(",")[0]);
-//		double y2 = Double.parseDouble(b.split(",")[1]);
-//		Wire wire = new Wire(x1+origin.getX(), y1+origin.getY(), x2+origin.getX(), y2+origin.getY(), wireColor);
-//		return wire;
-//	}
-
-//	private Junction junction(Node node) {
-//		String pos = getAttrValue(node, "pos");
-//		double x = Double.parseDouble(pos.split(",")[0]);
-//		double y = Double.parseDouble(pos.split(",")[1]);
-//		Junction junction = new Junction(x+origin.getX(), y+origin.getY());
-//		return junction;
-//	}
-
-//	private Pin pin(Node root, Node node) {
-//		String pos = getAttrValue(node, "pos");
-//		double x = Double.parseDouble(pos.split(",")[0]);
-//		double y = Double.parseDouble(pos.split(",")[1]);
-//		int directionCode = Integer.parseInt(getAttrValue(node, "direction"));
-//		Direction direction = Direction.get(directionCode);
-//		double length = Double.parseDouble(getAttrValue(node, "length"));
-//		String number = getAttrValue(node, "number");
-//		String name = node.getTextContent();
-//		//which='0' elec='0' part='0' show='3' number_pos='0' centre_name='0'
-//		Pin pin = new Pin(x+origin.getX(), y+origin.getY(), direction, length, number, name, pinColor, pinFont);
-//		return pin;
-//	}
-
-//	private Font getFont(Node node) {
-//		String name = getNode(node, "FACENAME").getTextContent();
-//		int style = Font.PLAIN;
-//		int size = Integer.parseInt(getNode(node, "WIDTH").getTextContent());
-//		Font font = new Font(name, style, size);
-//		return font;
 //	}
 
 	public static Bean getRef(List<Bean> refs, Class<? extends Bean> paramClass, String refId) {
