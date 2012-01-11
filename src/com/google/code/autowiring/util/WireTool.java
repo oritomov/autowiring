7080747968cf0faa901592e8bcb5b53ca763e444
oritomov <oritomov@ea482eac-fd92-57f8-a034-e20fc9fb603f>
2012-01-11 18:21:52 +0000

    fix wire tool package

--- src/com/google/code/autowiring/util/WireTool.java
+++ src/com/google/code/autowiring/util/WireTool.java
@@ -1,24 +1,25 @@
-package com.google.code.autowiring.tynicad;
+package com.google.code.autowiring.util;
 
 import java.util.Iterator;
 import java.util.List;
 
 import com.google.code.autowiring.Bean;
-import com.google.code.autowiring.tynicad.beans.Point;
-import com.google.code.autowiring.tynicad.beans.Wire;
+import com.google.code.autowiring.beans.Pnt;
+import com.google.code.autowiring.beans.Path;
 
 public class WireTool {
 
 	public static boolean cobineWire(List<Bean> beans, Bean bean2) {
 		boolean result = false;
-		if (bean2 instanceof Wire) {
+		if (bean2 instanceof Path) {
 			Iterator<Bean> i = beans.iterator();
 			while (i.hasNext()) {
 				Bean bean1 = i.next();
-				if (bean1 instanceof Wire) {
-					Wire wire1 = (Wire) bean1;
-					Wire wire2 = (Wire) bean2;
-					if ((wire1.getStrokeColor().equalsIgnoreCase(wire2.getStrokeColor())) && 
+				if (bean1 instanceof Path) {
+					Path wire1 = (Path) bean1;
+					Path wire2 = (Path) bean2;
+					if ((wire1.getStrokeColor() != null) &&
+						(wire1.getStrokeColor().equalsIgnoreCase(wire2.getStrokeColor())) && 
 						(wire1.getStrokeWidth() == wire2.getStrokeWidth()) && 
 						((wire1.getFillColor() == null) && (wire2.getFillColor() == null))) {
 						if (checkContWire(wire1, wire2)) {
@@ -56,10 +57,10 @@ public class WireTool {
 	 * @param wire1 B-C
 	 * @param wire2 A-B
 	 */
-	private static void insertWire(Wire wire1, Wire wire2) {
+	private static void insertWire(Path wire1, Path wire2) {
 		wire1.getPoints().remove(0);
 		for (int i = wire2.getPoints().size() - 1; i >= 0; i--) {
-			Point point = (Point) wire2.getPoints().get(i);
+			Pnt point = wire2.getPoints().get(i);
 			double x = point.getX() + wire1.getX() - wire2.getX();
 			double y = point.getY() + wire1.getY() - wire2.getY();
 			point.setX(x);
@@ -73,9 +74,9 @@ public class WireTool {
 	 * @param wire1 A-B
 	 * @param wire2 B-C
 	 */
-	private static void joinWires(Wire wire1, Wire wire2) {
+	private static void joinWires(Path wire1, Path wire2) {
 		for (int i = 1; i < wire2.getPoints().size(); i++) {
-			Point point = (Point) wire2.getPoints().get(i);
+			Pnt point = wire2.getPoints().get(i);
 			double x = point.getX() + wire1.getX() - wire2.getX();
 			double y = point.getY() + wire1.getY() - wire2.getY();
 			point.setX(x);
@@ -89,9 +90,9 @@ public class WireTool {
 	 * @param wire1 B-C
 	 * @param wire2 B-A
 	 */
-	private static void insertBackWire(Wire wire1, Wire wire2) {
+	private static void insertBackWire(Path wire1, Path wire2) {
 		for (int i = 1; i < wire2.getPoints().size(); i++) {
-			Point point = (Point) wire2.getPoints().get(i);
+			Pnt point = wire2.getPoints().get(i);
 			double x = point.getX() + wire1.getX() - wire2.getX();
 			double y = point.getY() + wire1.getY() - wire2.getY();
 			point.setX(x);
@@ -105,9 +106,9 @@ public class WireTool {
 	 * @param wire1 A-B
 	 * @param wire2 C-B
 	 */
-	private static void joinBackWires(Wire wire1, Wire wire2) {
+	private static void joinBackWires(Path wire1, Path wire2) {
 		for (int i = wire2.getPoints().size() - 2; i >= 0; i--) {
-			Point point = (Point) wire2.getPoints().get(i);
+			Pnt point = wire2.getPoints().get(i);
 			double x = point.getX() + wire1.getX() - wire2.getX();
 			double y = point.getY() + wire1.getY() - wire2.getY();
 			point.setX(x);
@@ -122,9 +123,9 @@ public class WireTool {
 	 * @param wire2 B..
 	 * @return true if wire1 ends with start of wire2 
 	 */
-	private static boolean checkContWire(Wire wire1, Wire wire2) {
-		Point p1last = (Point) wire1.getPoints().get(wire1.getPoints().size() - 1);
-		Point p2first = (Point) wire2.getPoints().get(0);
+	private static boolean checkContWire(Path wire1, Path wire2) {
+		Pnt p1last = wire1.getPoints().get(wire1.getPoints().size() - 1);
+		Pnt p2first = wire2.getPoints().get(0);
 		return ((p1last.getX()+wire1.getX()==p2first.getX()+wire2.getX()) && 
 				(p1last.getY()+wire1.getY()==p2first.getY()+wire2.getY()));
 	}
@@ -135,9 +136,9 @@ public class WireTool {
 	 * @param wire2 B..
 	 * @return true if wire1 and wire2 start on same point 
 	 */
-	private static boolean checkBeginWire(Wire wire1, Wire wire2) {
-		Point p1first = (Point) wire1.getPoints().get(0);
-		Point p2first = (Point) wire2.getPoints().get(0);
+	private static boolean checkBeginWire(Path wire1, Path wire2) {
+		Pnt p1first = wire1.getPoints().get(0);
+		Pnt p2first = wire2.getPoints().get(0);
 		return ((p1first.getX()+wire1.getX()==p2first.getX()+wire2.getX()) && 
 				(p1first.getY()+wire1.getY()==p2first.getY()+wire2.getY()));
 	}
@@ -148,9 +149,9 @@ public class WireTool {
 	 * @param wire2 B..
 	 * @return true if wire1 and wire2 end on same point 
 	 */
-	private static boolean checkEndWire(Wire wire1, Wire wire2) {
-		Point p1last = (Point) wire1.getPoints().get(wire1.getPoints().size() - 1);
-		Point p2last = (Point) wire2.getPoints().get(wire2.getPoints().size() - 1);
+	private static boolean checkEndWire(Path wire1, Path wire2) {
+		Pnt p1last = wire1.getPoints().get(wire1.getPoints().size() - 1);
+		Pnt p2last = wire2.getPoints().get(wire2.getPoints().size() - 1);
 		return ((p1last.getX()+wire1.getX()==p2last.getX()+wire2.getX()) && 
 				(p1last.getY()+wire1.getY()==p2last.getY()+wire2.getY()));
 	}
