13e86dcdea3979465dea84c4b095b3dae6239414
oritomov@gmail.com <oritomov@gmail.com@ea482eac-fd92-57f8-a034-e20fc9fb603f>
2012-01-21 15:29:09 +0000

    add symbol color

--- src/com/google/code/autowiring/tynicad/beans/Symbol.java
+++ src/com/google/code/autowiring/tynicad/beans/Symbol.java
@@ -1,6 +1,5 @@
 package com.google.code.autowiring.tynicad.beans;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import com.google.code.autowiring.Bean;
@@ -15,15 +14,16 @@ import com.google.code.autowiring.tynicad.TyniCAD;
  */
 public class Symbol extends Group {
 
+	private static final String COLOR = "color";
 	private SymbolDef def;
+	private String fieldColor;
+	private Font fieldFont;
 	private String color;
-	private Font font;
-	private List<Field> fields = new ArrayList<Field>();
 
 	public Symbol(List<Bean> refs) {
 		super();
-		setFont(refs, Pin.FONT_ID);
-		setColor(refs, Pin.COLOR_PIN);
+		setFieldFont(refs, Pin.FONT_ID);
+		setFieldColor(refs, Pin.COLOR_PIN);
 	}
 
 	public void setPos(Position pos) {
@@ -32,36 +32,38 @@ public class Symbol extends Group {
 	}
 	public void setId(SymbolDef def) {
 		this.def = def;
+		/** for all */
+		for (Bean bean: def.detBeans()) {
+			addBeans(TyniCAD.clone(bean));
+		}
 	}
-	private void setFont(List<Bean> refs, String refId) {
-		this.font = (Font) TyniCAD.getRef(refs, Font.class, refId);
+	private void setFieldFont(List<Bean> refs, String refId) {
+		this.fieldFont = (Font) TyniCAD.getRef(refs, Font.class, refId);
 	}
-	private void setColor(List<Bean> refs, String name) {
-		this.color = TyniCAD.getOption(refs, name);
+	private void setFieldColor(List<Bean> refs, String name) {
+		this.fieldColor = TyniCAD.getOption(refs, name);
+	}
+	public String getColor() {
+		return color;
 	}
 	public void addFields(Field field) {
-		fields.add(field);
-	}
-	@Override
-	public List<Bean> getBeans() {
-		List<Bean> beans = new ArrayList<Bean>();
-		for (Field field: fields) {
+		if (COLOR.equalsIgnoreCase(field.getDescription())) {
+			color = field.getValue();
+		} else {
 			if (field.getShow() > 0) {
 				Label label = new Label();
 				label.setX(field.getX()+def.getX());
 				label.setY(field.getY()+def.getY());
 				label.setLabel(field.getValue());
 				label.setDirection(Direction.Right);
-				label.setColor(color);
-				label.setFont(font);
+				label.setColor(fieldColor);
+				label.setFont(fieldFont);
 				if (label.getFontSize() == 0) {
 					label.setFontSize(Pin.FONT_SIZE);
 				}
-				beans.add(label);
+				addBeans(label);
 			}
 		}
-		beans.addAll(def.detBeans());
-		return beans;
 	}
 	public double getOffsX() {
 		return getX() - def.getX();
