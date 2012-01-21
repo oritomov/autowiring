d598cade5b53fcc2fd935f59dd1c71e6651e7fc1
oritomov@gmail.com <oritomov@gmail.com@ea482eac-fd92-57f8-a034-e20fc9fb603f>
2012-01-21 16:48:40 +0000

    symbol rotate (initial)

--- src/com/google/code/autowiring/beans/Group.java
+++ src/com/google/code/autowiring/beans/Group.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import com.google.code.autowiring.Bean;
+import com.google.code.autowiring.Wiring.Direction;
 
 /**
  * @author	Orlin Tomov
@@ -15,6 +16,8 @@ public class Group  implements Bean {
 	private double x;
 	private double y;
 	private List<Bean> beans;
+	private Direction direction;
+	private boolean mirror;
 
 	protected Group() {
 		super();
@@ -33,15 +36,33 @@ public class Group  implements Bean {
 	public void setY(double y) {
 		this.y = y;
 	}
+	public boolean getMirror() {
+		return mirror;
+	}
+	public void setMirror(boolean mirror) {
+		this.mirror = mirror;
+	}
+	public Direction getDirection() {
+		return direction;
+	}
+	public void setDirection(Direction direction) {
+		this.direction = direction;
+	}
 	public List<Bean> getBeans() {
 		return beans;
 	}
 	public void addBeans(Bean bean) {
 		beans.add(bean);
 	}
-
 	public String getTransform() {
 		String transform = "translate("+getX()+","+getY()+")";
 		return transform;
 	}
+	// 558.0,154.0
+	// 90 matrix(0,1,-1,0,715.0481,-408.9519)
+	//180 matrix(-1,0,0,-1,1124,306.0962)
+	//270 matrix(0,-1,1,0,408.9519,715.0481)
+	//mirror
+	// matrix(-1,0,0,1,1124,0)
+	
 }
--- src/com/google/code/autowiring/tynicad/beans/Symbol.java
+++ src/com/google/code/autowiring/tynicad/beans/Symbol.java
@@ -37,6 +37,13 @@ public class Symbol extends Group {
 			addBeans(TyniCAD.clone(bean));
 		}
 	}
+	public void setRotate(String rotate) {
+		int code = Integer.parseInt(rotate);
+		Direction direction = Direction.get(code % Direction.values().length);
+		setDirection(direction);
+		setMirror((code / Direction.values().length) > 0);
+		
+	}
 	private void setFieldFont(List<Bean> refs, String refId) {
 		this.fieldFont = (Font) TyniCAD.getRef(refs, Font.class, refId);
 	}
